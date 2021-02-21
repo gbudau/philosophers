06ab56e299c7d6d0a5a210c660a664a58240def5
gbudau <54190400+gbudau@users.noreply.github.com>
2021-02-22 00:14:32 +0100

    [REFACTOR] philo_three
    Splitted create_philo_proc functions into three functions
    Removed debug printfs

--- philo_three/src/utils.c
+++ philo_three/src/utils.c
@@ -6,7 +6,7 @@
 /*   By: gbudau <gbudau@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/15 17:04:46 by gbudau            #+#    #+#             */
-/*   Updated: 2021/02/21 23:52:36 by gbudau           ###   ########.fr       */
+/*   Updated: 2021/02/22 00:11:09 by gbudau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,51 @@ void		clean_all_philos(pid_t	*philos, unsigned count)
 		kill(philos[i++], SIGKILL);
 }
 
+void		open_philosopher_sem(t_philo *ph, t_monitor *mon)
+{
+	int		oflag;
+	int		mode;
+	char	*sem_name;
+
+	oflag = O_CREAT | O_EXCL;
+	mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
+	mon->print_status = sem_open("/print_status", oflag, mode, 1);
+	sem_unlink("/print_status");
+	ph->print_status = mon->print_status;
+	sem_name = create_sem_name("/check_starvation", ph->id);
+	mon->check_starvation = sem_open(sem_name, oflag, mode, 1);
+	sem_unlink(sem_name);
+	ph->check_starvation = mon->check_starvation;
+	sem_name = create_sem_name("/check_dining_complete", ph->id);
+	mon->check_dining_complete = sem_open(sem_name, oflag, mode, 1);
+	sem_unlink(sem_name);
+	ph->check_dining_complete = mon->check_dining_complete;
+	sem_name = create_sem_name("/check_has_forks", ph->id);
+	mon->check_has_forks = sem_open(sem_name, oflag, mode, 1);
+	sem_unlink(sem_name);
+	ph->check_has_forks = mon->check_has_forks;
+}
+
+void		run_philosopher(sem_t *forks, t_args *args, unsigned id,
+											t_monitor_dining_complete *mon_dc)
+{
+	t_philo		ph;
+	t_monitor	mon;
+
+	memset(&ph, 0, sizeof(ph));
+	ph.id = id;
+	ph.args = args;
+	ph.forks = forks;
+	ph.last_eat_time = args->start_time;
+	open_philosopher_sem(&ph, &mon);
+	ph.dining_complete = mon_dc->dining_complete;
+	mon.ph = &ph;
+	mon.args = args;
+	pthread_create(&mon.thread, NULL, &monitor_self, &mon);
+	pthread_detach(mon.thread);
+	dine_philo(&ph);
+}
+
 void		create_philo_proc(sem_t *forks, t_args *args, pid_t *philos,
 											t_monitor_dining_complete *mon_dc)
 {
@@ -60,70 +105,7 @@ void		create_philo_proc(sem_t *forks, t_args *args, pid_t *philos,
 			exit(1);
 		}
 		if (pid == 0)
-		{
-			// TODO This variables are used only in the child process
-			t_philo		ph;
-			char		*sem_name;
-			t_monitor	mon;
-
-			memset(&ph, 0, sizeof(ph));
-			ph.id = i + 1;
-			ph.args = args;
-			ph.forks = forks;
-			ph.last_eat_time = args->start_time;
-			// Semaphore used to protect writing on stdout after a philosopher dies
-			mon.print_status = sem_open("/print_status", O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH, 1);
-			sem_unlink("/print_status");
-			ph.print_status = mon.print_status;
-			// Semaphore used to check if current philosopher is starving
-			sem_name = create_sem_name("/check_starvation", ph.id);
-			mon.check_starvation = sem_open(sem_name, O_CREAT | O_EXCL, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH, 1);
-			sem_unlink(sem_name);
-			ph.check_starvation = mon.check_starvation;
-			// Semaphore used to check if current philosopher has eat atleast number_of_times_each_philo_must_eat
-			sem_name = create_sem_name("/check_dining_complete", ph.id);
-			mon.check_dining_complete = sem_open(sem_name, O_CREAT | O_EXCL, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH, 1);
-			sem_unlink(sem_name);
-			ph.check_dining_complete = mon.check_dining_complete;
-			// Semaphore used to check how many forks the current philosopher picked up
-			sem_name = create_sem_name("/check_has_forks", ph.id);
-			mon.check_has_forks = sem_open(sem_name, O_CREAT | O_EXCL, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH, 1);
-			sem_unlink(sem_name);
-			ph.check_has_forks = mon.check_has_forks;
-			// Semaphore used to signal when current philosopher has eat atleast number_of_times_each_philo_must_eat to the parent process
-			ph.dining_complete = mon_dc[i].dining_complete;
-#if 0
-
-			sem_name = create_sem_name("/dining_complete", ph.id);
-			sem_unlink(sem_name);
-			// TODO remove errno
-			errno = 0;
-
-			printf("CHILD %s\n", sem_name);
-			if ((ph.dining_complete = sem_open(sem_name, 0)) == SEM_FAILED)
-				if (errno)
-				{
-					perror("Child semaphore");
-					exit(1);
-				}
-
-			// TODO Remove this
-			if (ph.dining_complete != SEM_FAILED)
-			{
-				int test_value;
-				sem_getvalue(ph.dining_complete, &test_value);
-				printf("AAA Sem value is %d\n", test_value);
-			}
-
-			sem_unlink(sem_name);
-#endif
-
-			mon.ph = &ph;
-			mon.args = args;
-			pthread_create(&mon.thread, NULL, &monitor_self, &mon);
-			pthread_detach(mon.thread);
-			dine_philo(&ph);
-		}
+			run_philosopher(forks, args, i + 1, &mon_dc[i]);
 		philos[i] = pid;
 		i++;
 	}
