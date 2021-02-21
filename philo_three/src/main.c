7a46dd389534354ba637598c8097f22cb8929fc6
gbudau <54190400+gbudau@users.noreply.github.com>
2021-02-21 23:39:28 +0100

    [REFACTOR] philo_three
    Created a function to allocate the memory instead of doing it in main

--- philo_three/src/main.c
+++ philo_three/src/main.c
@@ -6,7 +6,7 @@
 /*   By: gbudau <gbudau@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/16 20:55:15 by gbudau            #+#    #+#             */
-/*   Updated: 2021/02/21 23:12:16 by gbudau           ###   ########.fr       */
+/*   Updated: 2021/02/21 23:38:34 by gbudau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -193,6 +193,25 @@ void		create_and_detach_monitor_threads(t_args *args,
 	pthread_detach(thread_id);
 }
 
+int			allocate_memory(pid_t **philos, t_args *args,
+									t_super_monitor_dining_complete *super_mon,
+											t_monitor_dining_complete **mon_dc)
+{
+	*philos = malloc(sizeof(pid_t) * args->n_philos);
+	if (philos == NULL)
+		return (-1);
+	super_mon->is_dining_complete = malloc(sizeof(unsigned) * args->n_philos);
+	if (super_mon->is_dining_complete == NULL)
+		return (-1);
+	super_mon->lock_dining_complete = malloc(sizeof(sem_t *) * args->n_philos);
+	if (super_mon->lock_dining_complete == NULL)
+		return (-1);
+	*mon_dc = malloc(sizeof(t_monitor_dining_complete) * args->n_philos);
+	if (mon_dc == NULL)
+		return (-1);
+	return (0);
+}
+
 int			main(int argc, char **argv)
 {
 	t_args							args;
@@ -202,28 +221,16 @@ int			main(int argc, char **argv)
 	t_monitor_dining_complete		*mon_dc;
 
 	memset(&args, 0, sizeof(args));
+	memset(&super_mon, 0, sizeof(super_mon));
 	if (check_args(argc, argv, &args) == -1)
 		return (1);
-	philos = malloc(sizeof(pid_t) * args.n_philos);
-	if (philos == NULL)
-		return (1);
-	super_mon.is_dining_complete = malloc(sizeof(unsigned) * args.n_philos);
-	if (super_mon.is_dining_complete == NULL)
-		return (1);
-	super_mon.lock_dining_complete = malloc(sizeof(sem_t *) * args.n_philos);
-	if (super_mon.lock_dining_complete == NULL)
-		return (1);
-	mon_dc = malloc(sizeof(t_monitor_dining_complete) * args.n_philos);
-	if (mon_dc == NULL)
+	if (allocate_memory(&philos, &args, &super_mon, &mon_dc) == -1)
 		return (1);
 	open_semaphores(&forks, &args, mon_dc);
-
 	create_philo_proc(forks, &args, philos, mon_dc);
-
 	super_mon.args = &args;
 	super_mon.philos = philos;
 	create_and_detach_monitor_threads(&args, &super_mon, mon_dc);
-
 	wait_all_philos(philos, &args);
 	return (0);
 }
